07831a73a351729450e8e8bb2e6bd80f5ed44075
Warathep Jamsri <warathepj@gmail.com>
2025-06-01 11:44:34 +0700

    html render msg ok

--- subscriber.js
+++ subscriber.js
@@ -1,5 +1,21 @@
 const mqtt = require('mqtt');
+const WebSocket = require('ws');
+
 const client = mqtt.connect('mqtt://localhost:1883');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+let clients = [];
+
+wss.on('connection', ws => {
+  console.log('Client connected');
+  clients.push(ws);
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clients = clients.filter(c => c !== ws);
+  });
+});
 
 client.on('connect', () => {
   client.subscribe('topic/test', (err) => {
@@ -11,5 +27,15 @@ client.on('connect', () => {
 
 client.on('message', (topic, message) => {
   // message is Buffer
-  console.log(message.toString());
+  const msg = message.toString();
+  console.log(`Received MQTT message: ${msg}`);
+  
+  // Send message to all connected WebSocket clients
+  clients.forEach(ws => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(msg);
+    }
+  });
 });
+
+console.log('MQTT Subscriber and WebSocket server started on port 8080');
